fe999ef42a45a5646278e416b0794146a70b9ffd
Eduardo Ney <dudolbh@gmail.com>
2021-11-27 20:10:16 -0300

    Inclusão gerador dados.

---
+++ app/libs/json_kafka.py
@@ -0,0 +1,65 @@
+# import libraries
+import json
+from confluent_kafka import Producer
+from libs import read_files
+
+
+class KafkaProducer():
+
+    def delivery_report(self, err, msg):
+        """called once for each message produced to indicate delivery result. triggered by poll() or flush()."""
+        if err is not None:
+            print('message delivery failed: {}'.format(err))
+        else:
+            print('message successfully produced to {} [{}] at offset {}'.format(msg.topic(), msg.partition(), msg.offset()))
+
+    # get data to insert from (read_files.csv)
+    # print(type(CSV().csv_reader()))
+    # <class 'list'>
+
+    def json_producer(self, broker, topic, dataframe):
+        """responsible to send data to kafka in json using utf-8"""
+
+        # producer configuration
+        p = Producer({
+            'client.id': 'json-python-stream-app',
+            'bootstrap.servers': broker,
+            "batch.num.messages": 100,
+            "queue.buffering.max.ms": 100,
+            "queue.buffering.max.messages": 1000,
+            "linger.ms": 200
+        })
+
+        # get data to be inserted
+        get_data = dataframe.to_dict(orient='records')
+
+        # for loop to insert all data
+        for data in get_data:
+
+            try:
+                # trigger any available delivery report callbacks from previous produce() calls
+                p.poll(0)
+
+                # asynchronously produce a message, the delivery report callback
+                # will be triggered from poll() above, or flush() below, when the message has
+                # been successfully delivered or failed permanently.
+                p.produce(
+                    topic=topic,
+                    value=json.dumps(data,default=str).encode('utf-8'),
+                    callback=self.delivery_report
+                )
+
+            except BufferError:
+                print("buffer full")
+                p.poll(0.1)
+
+            except ValueError:
+                print("invalid input")
+                raise
+
+            except KeyboardInterrupt:
+                raise
+
+        # wait for any outstanding messages to be delivered and delivery report
+        # callbacks to be triggered.
+        p.flush()
---
+++ app/main.py
@@ -0,0 +1,28 @@
+import argparse
+import sys
+from libs import json_kafka
+from libs import make_data
+# main
+if __name__ == '__main__':
+
+    # instantiate arg parse
+    parser = argparse.ArgumentParser(description='streaming app')
+
+    # add parameters to arg parse
+    parser.add_argument('stream', type=str, choices=['confluent', 'kafka'], help='stream processing')
+    parser.add_argument('broker', type=str, help='apache kafka broker')
+    parser.add_argument('topic', type=str, help='topic name')
+    parser.add_argument('qty_rows', type=int, help='amount of events')
+
+    # invoke help if null
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+    data = make_data.generate_data(args.qty_rows)
+
+    json_kafka.KafkaProducer().json_producer(broker=args.broker,topic=args.topic, dataframe=data)
+        
+        
+    # cli call
+    # python3.8 main.py
+    # python3.8 main.py 'kafka' 'dev-kafka-confluent.eastus2.cloudapp.azure.com' 'src-app-music-data-json' 10
+    # python3.8 main.py 'confluent' 'dev-kafka-confluent.eastus2.cloudapp.azure.com' 'http://dev-kafka-confluent.eastus2.cloudapp.azure.com:8081' 'src-app-music-data-avro' 10
